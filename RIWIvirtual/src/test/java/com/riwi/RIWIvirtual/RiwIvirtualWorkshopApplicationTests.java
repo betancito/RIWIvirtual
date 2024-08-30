83daaf5a277ae516c35e712027fdc014111c707b
betancito <163953475+betancito@users.noreply.github.com>
2024-08-29 21:22:05 -0500

    Uploading necessary files for proyect
    Uploading necessary schema for starting the workshop

---
+++ RIWIvirtual/src/test/java/com/riwi/RIWIvirtual/RiwIvirtualWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.RIWIvirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwIvirtualWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
